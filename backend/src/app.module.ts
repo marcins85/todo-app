37817551054d6abe0870a708c6e2d772236a08e4
Marcin Stachowicz <stassimon@gmail.com>
2025-08-31 08:54:59 +0200

    dodanie testów integracyjnych

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,7 +11,7 @@ import { TodosModule } from './todos/todos.module';
       port: 5432,
       username: 'postgres',
       password: '1234',
-      database: 'todo_app',
+      database: process.env.NODE_ENV === 'test' ? 'todo_test_app' : 'todo_app',
       entities: [Todo],
       synchronize: true, // tylko w dev!
     }),
---
+++ backend/test/todo.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { AppModule } from "../src/app.module";
+
+describe('TodoController (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
+        // await repository.clear();
+    });
+
+    it('/todos (GET) powinno zwrócić listę zadań', async () => {
+        const res = await request(app.getHttpServer())
+            .get('/todos');
+
+        expect(res.status).toBe(200);
+        expect(Array.isArray(res.body)).toBe(true);
+    });
+
+    it('/todos (POST) powinno dodać zadanie', async () => {
+        const res = await request(app.getHttpServer())
+            .post('/todos')
+            .send({ title: 'Testowe zadanie' });
+
+        expect(res.status).toBe(201);
+        expect(res.body).toHaveProperty('id');
+        expect(res.body.text).toBe('Testowe zadanie');
+    });
+});
